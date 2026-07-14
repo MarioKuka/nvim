da4a196ba08daffa9c3695afd889a336a556db25
Mario Kuka <kuka@cesnet.cz>
2026-07-14 22:12:50 +0200

    nvim map ctrl+v and ctrl+c

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Ctrl+C to copy (visual mode) and yank in normal mode
+vim.keymap.set({ "v", "n" }, "<C-c>", '"+y', { desc = "Copy to clipboard" })
+-- Ctrl+V to paste
+vim.keymap.set({ "v", "n", "i" }, "<C-v>", '"+p', { desc = "Paste from clipboard" })
+-- In terminal mode, paste from clipboard
+vim.keymap.set("t", "<C-v>", "<C-\\><C-n>\"+p", { desc = "Paste in terminal" })
